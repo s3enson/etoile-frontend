f59f1c95a8870bb5e8a7531071d6ee2b6bb1dd7c
s3enson <szymon.gustek@gmail.com>
2026-05-16 23:48:21 +0200

    fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Creator from './creator/Creator.jsx'
-import Blog from './blog/Blog.jsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -12,7 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/creator" element={<Creator />} />
-        <Route path="/blog/*" element={<Blog />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
